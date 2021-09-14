da79eedf0235f2cd954413d193873ba81ffffc8b
Dragan Cicic <your3mail>
2021-09-14 18:06:55 -0400

    pusling

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function CountryDetails(props) {
-	const [ air, setAir ] = useState({});
+  const [CountryDetails, setCountryDetails] = useState({});
 
-	let [ city, setCity ] = useState('Miami');
-	let [ state, setState ] = useState('Florida');
-	let [ country, setCountry ] = useState('USA');
+  let [city, setCity] = useState();
+  let [state, setState] = useState();
+  let [country, setCountry] = useState();
 
-	useEffect(() => {
-		axios
-			.get(
-				`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=6f4c381f-9e57-4316-82df-7d1c53d6b9a2`
-			)
-			.then((resApi) => {
-				console.log(resApi);
-				setAir(resApi.data.data);
-			});
-	}, []);
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=6f4c381f-9e57-4316-82df-7d1c53d6b9a2`
+      )
+      .then((resApi) => {
+        console.log(resApi);
+        setCountryDetails(resApi.data.data);
+      });
+  }, []);
 
-	const CountryDetails = () => {
-		return <div>Country Details</div>;
-	};
+  const ShowCountryDetails = () => {
+    return (
+      <div className="CountryDetails">
+        <div>
+          <h2>{}</h2>
+
+          <h2>hello</h2>
+          <h2>hello</h2>
+        </div>
+      </div>
+    );
+  };
 }
 export default CountryDetails;
